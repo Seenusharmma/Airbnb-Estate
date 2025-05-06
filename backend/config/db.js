94a7981d2b1b570b9b9862d7b9bca511fd1aa0be
Roshan Sharma <143079337+Seenusharmma@users.noreply.github.com>
2025-05-06 13:30:59 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect("mongodb+srv://roshansharma404error:YZxYf55Io4jBpgXk@cluster0.t6gnssl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("DB connected")
     } catch (error) {
         console.log("db error")
